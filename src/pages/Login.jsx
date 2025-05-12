5cd1194bfe8cbbbefea31594764f482cd4270903
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-05-12 20:43:14 +0600

    login page complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,69 @@
 import React from "react";
-
+import loginbg from "../assets/portal_login-bg__sp.png";
+import logo from "../assets/logo-footer.svg";
+import NavWIthoutLinks from "../components/shared/navWIthoutLinks";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
   return (
     <div>
-      <h2>This is login page</h2>
+      <NavWIthoutLinks />
+      <div className="  grid grid-cols-1 md:grid-cols-2 mt-28">
+        <div className="flex justify-center items-center container p-4">
+          <div className="bg-white md:bg-gray-100 p-8 rounded-lg">
+            <h2 className="font-bold text-3xl text-emerald-600 mb-2">Login</h2>
+            <p className="text-gray-500 text-xl">
+              Log in to access your Safeport Law portal
+            </p>
+            <form>
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="border border-gray-300 rounded-lg w-full p-4 my-4 "
+              />
+              <div className="relative mb-4">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  className="border border-gray-300 rounded-lg w-full p-4 "
+                />
+
+                <span className="absolute text-gray-600 w-5 h-5 right-4 top-1/2 transform -translate-y-1/2  2">
+                  {!showPassword ? (
+                    <LuEyeClosed
+                      onClick={() => setShowPassword(!showPassword)}
+                    />
+                  ) : (
+                    <LuEye onClick={() => setShowPassword(!showPassword)} />
+                  )}
+                </span>
+              </div>
+
+              <p className="mt-2 mb-12">Forgot Password?</p>
+              <button className="btn btn-secondary w-full">Login</button>
+            </form>
+            <p className="text-center mt-4">
+              Don&#8217;t have an account?{" "}
+              <span className=" font-bold ">Create account now</span>
+            </p>
+          </div>
+        </div>
+
+        <div className="hidden md:block relative h-[100vh]">
+          <img src={loginbg} alt="logoin background image " />
+          <div className="absolute bg-black inset-0 opacity-50"></div>
+          <div className="absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <h2 className="font-bold text-4xl text-white mb-2">
+              With real attorneys on your side, you’re in good hands.
+            </h2>
+            <p className="text-xs text-white">
+              At Safeport Law, we have a team of attorneys who are dedicated to
+              helping you.
+            </p>
+            <img src={logo} alt="logo" className="w-48 mt-8" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
